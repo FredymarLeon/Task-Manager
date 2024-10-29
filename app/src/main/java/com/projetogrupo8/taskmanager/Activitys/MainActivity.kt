f70b502f2ed74ecc011c0652f4c015189cdb2873
FredymarLeon <fredymar@gmail.com>
2024-10-29 11:40:35 -0300

    Implementação de navegação utilizando Intent: Navegação da MainActivity para SecondActivity.

--- app/src/main/java/com/projetogrupo8/taskmanager/Activitys/MainActivity.kt
+++ app/src/main/java/com/projetogrupo8/taskmanager/Activitys/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.projetogrupo8.taskmanager.Activitys
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 import com.projetogrupo8.taskmanager.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: MaterialButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +22,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setupButton()
+    }
+
+    private fun setupButton() {
+        button = findViewById(R.id.btnLogin)
+        button.setOnClickListener {
+            handleButtonClick()
+        }
+    }
+
+    private fun handleButtonClick() {
+        val intent = Intent(applicationContext, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
