804ed2e709a34b18ea450d25862d3883d62ffe6a
FredymarLeon <fredymar@gmail.com>
2024-10-31 22:18:30 -0300

    Foi finalizada a criação das telas de Listar, criar e editar tarefas.
    Se criou o theme do app.
    
    Falta definir imagem para mostrar quando a tela de listagem de tarefas estiver vacia (png)
    
    Falta criar </>Strings.xml em espanhol e ingles.

--- app/src/main/java/com/projetogrupo8/taskmanager/activities/SecondActivity.kt
+++ app/src/main/java/com/projetogrupo8/taskmanager/activities/SecondActivity.kt
@@ -6,9 +6,11 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.projetogrupo8.taskmanager.R
+import com.projetogrupo8.taskmanager.fragments.TaskManagerFragment
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/projetogrupo8/taskmanager/fragments/TaskManagerFragment.kt
+++ app/src/main/java/com/projetogrupo8/taskmanager/fragments/TaskManagerFragment.kt
@@ -18,4 +18,8 @@ class TaskManagerFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_task_manager, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
